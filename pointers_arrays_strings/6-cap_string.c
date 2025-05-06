ecdfa5d0c8a4881fb530742a93f110dbad125e47
siham10927 <10927@holbertonstudents.com>
2025-05-06 18:52:02 +0000

    Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,36 @@
 #include "main.h"
-
 /**
- * cap_string - Write a function that capitalizes all words of a string.
+ * cap_string - capitalizes most of the words in a string.
+ * @s: analized string.
  *
- * @entry: This is the input string
- *
- * Return: String capitalized
+ * Return: String with all words capitalized.
  */
-char *cap_string(char *entry)
+char *cap_string(char *s)
 {
-	int conversion, index, count;
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	char chars[] = {' ', ',', ';', '.', '!',
-			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
-	conversion = 32;
-
-	for (index = 0; entry[index] != '\0'; index++)
+	i = 0;
+	while (*(s + i) != '\0')
 	{
-		if (entry[index] >= 'index' && entry[index] <= 'z')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			entry[index] =  entry[index] - conversion;
-		}
-		conversion = 0;
-		for (count = 0; chars[count] != '\0'; count++)
-		{
-			if (chars[count] == entry[index])
+			if (i == 0)
 			{
-				conversion = 32;
-				break;
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) = *(s + i) - 32;
+					}
+				}
 			}
 		}
+	i++;
 	}
-	return (entry);
+	return (s);
 }
