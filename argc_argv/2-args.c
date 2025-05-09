9bb1b9678d9a831bc381c0c66029a6e63cae2479
siham10927 <10927@holbertonstudents.com>
2025-05-09 15:25:19 +0000

    The best argument against democracy is a five-minute conversation with the average voter

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the arguments plus the application name.
+ * @argc: count.
+ * @argv: string with the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
+
+
