d8d07c3779a2e329d9078edca6c241f840fdae18
siham10927 <10927@holbertonstudents.com>
2025-05-09 15:15:25 +0000

    It ain't what they call you, it's what you answer to

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,16 +1,17 @@
 #include <main.h>
+#include <stdio.h>
 
 /**
- * main - Entry point.
- * @argc: size of argv
- * @argv: name of app + any other arguments.
- *
- * Return: Alway 0 (sucess)
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc * 0]);
+
 	return (0);
 }
-
 
